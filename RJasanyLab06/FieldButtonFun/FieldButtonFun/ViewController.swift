436060841470db3973cc9684764b7ea356dedc15
Ryan <ryanjasany6@gmail.com>
2018-02-20 16:39:49 -0800

    Hour 7 code

--- RJasanyLab06/FieldButtonFun/FieldButtonFun/ViewController.swift
+++ RJasanyLab06/FieldButtonFun/FieldButtonFun/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var thePlace: UITextField!
+    
+    @IBOutlet weak var theVerb: UITextField!
+    
+    @IBOutlet weak var theNumber: UITextField!
+    
+    
+    @IBOutlet weak var theTemplate: UITextView!
+    
+    
+    @IBOutlet weak var theStory: UITextView!
+    
+    
+    @IBAction func createStory(_ sender: Any) {
+        
+        theStory.text=theTemplate.text
+        theStory.text=theStory.text.replacingOccurrences(of:"<place>", with:thePlace.text!)
+        theStory.text=theStory.text.replacingOccurrences(of:"<verb>", with:theVerb.text!)
+        theStory.text=theStory.text.replacingOccurrences(of:"<number>", with:theNumber.text!)    }
+    @IBAction func hideKeyboard(_ sender: Any) {
+        
+        thePlace.resignFirstResponder()
+        theVerb.resignFirstResponder()
+        theNumber.resignFirstResponder()
+        theTemplate.resignFirstResponder()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
