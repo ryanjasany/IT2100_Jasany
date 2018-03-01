cd2a198752fb2d0cf225a74dbd7f92f6cfc5ab7f
Ryan <ryanjasany6@gmail.com>
2018-02-28 16:50:06 -0800

    finish Lab07RJasany

--- Lab07RJASANY/Lab07RJasany/Lab07RJasany/ViewController.swift
+++ Lab07RJASANY/Lab07RJasany/Lab07RJasany/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    @IBOutlet weak var stepperValue: UIStepper!
+    
+    @IBOutlet weak var sliderOutput: UILabel!
+    
+    @IBOutlet weak var stepperOutput: UILabel!
+    
+    @IBAction func changeSlider(_ sender: Any) {
+        
+        sliderOutput.text="Slider: "+String(format: "%.0f",sliderValue.value)
+        
+    }
+    
+    @IBAction func changeStepper(_ sender: Any) {
+        
+        stepperOutput.text="Stepper: "+String(format: "%.0f", stepperValue.value)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
