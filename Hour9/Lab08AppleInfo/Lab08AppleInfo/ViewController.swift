dda26e36f7b5af1ec292254dbf7d9e8dc8cad99e
Ryan <ryanjasany6@gmail.com>
2018-03-06 16:27:49 -0800

    Changes for Lab 08

--- Hour9/Lab08AppleInfo/Lab08AppleInfo/ViewController.swift
+++ Hour9/Lab08AppleInfo/Lab08AppleInfo/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appleLink: UISegmentedControl!
+    @IBOutlet weak var webApple: WKWebView!
+    
+    @IBAction func changeAppleLink(_ sender: Any) {
+        var appleURLString = ""
+        switch appleLink.selectedSegmentIndex {
+        case 0:
+            appleURLString = "https:c.//en.wikipedia.org/wiki/Apple_Inc."
+        case 1:
+            appleURLString = "https://en.wikipedia.org/wiki/IOS"
+        case 2:
+            appleURLString = "https://en.wikipedia.org/wiki/Xcode"
+        case 3:
+            appleURLString = "https://en.wikipedia.org/wiki/Swift_(programming_language)"
+        default:
+            appleURLString = "https://en.wikipedia.org/wiki/Apple_In"
+        }
+        let myURL = URL(string: appleURLString)
+        let myRequest = URLRequest(url: myURL!)
+        webApple.load(myRequest)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let myURL = URL(string: "https://en.wikipedia.org/wiki/Apple_Inc.")
+        let myRequest = URLRequest(url: myURL!)
+        webApple.load(myRequest)
     }
 
     override func didReceiveMemoryWarning() {
