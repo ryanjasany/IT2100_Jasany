10f505cc857bc22397798532ca3a9619cbaa0eb4
Ryan <ryanjasany6@gmail.com>
2018-04-09 18:44:06 -0400

    Lets Navigate

---
+++ Hour 13/LetsNavigate/LetsNavigate/CountingNavigationControllerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CountingNavigationControllerViewController.swift
+//  LetsNavigate
+//
+//  Created by user135477 on 4/9/18.
+//  Copyright © 2018 Ryan Jasany. All rights reserved.
+//
+
+import UIKit
+
+class CountingNavigationControllerViewController: UINavigationController {
+
+    var pushCount:Int = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
