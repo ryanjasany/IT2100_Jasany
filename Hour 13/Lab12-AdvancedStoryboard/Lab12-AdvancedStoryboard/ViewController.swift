ca1d35a9d5c76809cf7eb0d1c4bca521471ba4c5
Ryan <ryanjasany6@gmail.com>
2018-04-14 10:07:09 -0400

    init commit lab 12

---
+++ Hour 13/Lab12-AdvancedStoryboard/Lab12-AdvancedStoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab12-AdvancedStoryboard
+//
+//  Created by user135477 on 4/14/18.
+//  Copyright © 2018 Ryan Jasany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
