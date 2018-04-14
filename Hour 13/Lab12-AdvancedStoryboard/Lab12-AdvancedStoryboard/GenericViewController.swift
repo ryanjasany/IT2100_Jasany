93c04522b632f21208edff99c6cee7b5fc632f1b
Ryan <ryanjasany6@gmail.com>
2018-04-14 10:17:40 -0400

    add view controllers

---
+++ Hour 13/Lab12-AdvancedStoryboard/Lab12-AdvancedStoryboard/GenericViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GenericViewController.swift
+//  Lab12-AdvancedStoryboard
+//
+//  Created by user135477 on 4/14/18.
+//  Copyright © 2018 Ryan Jasany. All rights reserved.
+//
+
+import UIKit
+
+class GenericViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
