30715dffd59af3814136fa62dccc4bc979d1e18a
Ryan <ryanjasany6@gmail.com>
2018-04-16 18:55:40 -0400

    Hour 14 table view

--- Hour 14/FlowerColorTable/FlowerColorTable/ViewController.swift
+++ Hour 14/FlowerColorTable/FlowerColorTable/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
 
+    
+
+    let redFlowers: [String] = ["Gerbera","Peony","Rose","Poppy"]
+    
+    let blueFlowers: [String] = ["Hyacinth","Hydrangea","Sea Holly","Phlox","Iris"]
+    
+    let kSectionCount:Int = 2
+    let kRedSection:Int = 0
+    let kBlueSection:Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,62 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return kSectionCount
+    }
+    
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int{
+        
+        switch section{
+        case kRedSection:
+            return redFlowers.count
+        case kBlueSection:
+            return blueFlowers.count
+        default:
+            return 0
+            
+            
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section:  Int) -> String?{
+        
+        switch section{
+        case kRedSection:
+            return "Red"
+        case kBlueSection:
+            return "Blue"
+        default:
+            return "Unknown"
+            
+        }
+    
+    }
 
+    func tableView(_ tableView:UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "flowerCell")! as UITableViewCell
+        
+        switch(indexPath.section){
+        case kRedSection:
+            cell.textLabel!.text = redFlowers[indexPath.row]
+        case kRedSection:
+            cell.textLabel!.text = blueFlowers[indexPath.row]
+        default:
+            cell.textLabel!.text = "Unknown"
+            
+        }
+        
+        let flowerImage: UIImage = UIImage(named: cell.textLabel!.text!)!
+        
+        cell.imageView!.image = flowerImage
+        
+        return cell
+        
+    }
+    
 
 }
 
