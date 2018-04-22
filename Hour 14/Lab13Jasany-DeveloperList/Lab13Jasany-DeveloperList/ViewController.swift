5f5a3c7574e9c51c4ca4a80f90de0ed2c294d3f1
Ryan <ryanjasany6@gmail.com>
2018-04-22 09:57:39 -0400

    implement data source protocol

--- Hour 14/Lab13Jasany-DeveloperList/Lab13Jasany-DeveloperList/ViewController.swift
+++ Hour 14/Lab13Jasany-DeveloperList/Lab13Jasany-DeveloperList/ViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let webDevelopers: [String] = ["Al Dente", "Armand Hammer", "Barry Cade", "Cara Van"]
+    let mobileDevelopers: [String] = ["Ginger Ayle", "June Bugg", "Iona Ford", "Sue Flay", "Tim Burr", "Will Wynn"]
+    
+    let kSectionCount:Int = 2
+    let kWebSection:Int = 0
+    let kMobileSection:Int = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return kSectionCount
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section{
+        case kWebSection:
+            return webDevelopers.count
+        case kMobileSection:
+            return mobileDevelopers.count
+        default:
+            return 0
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?{
+        switch section{
+        case kWebSection:
+            return "Web Developers"
+        case kMobileSection:
+            return "Mobile Developers"
+        default:
+            return "Unknown"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "developerCell")! as UITableViewCell
+        
+        switch(indexPath.section){
+        case kWebSection:
+            cell.textLabel!.text = webDevelopers[indexPath.row]
+        case kMobileSection:
+            cell.textLabel!.text = mobileDevelopers[indexPath.row]
+        default:
+            cell.textLabel!.text = "Unknown"
+        }
+        
+        let developerImage: UIImage = UIImage(named: cell.textLabel!.text!)!
+        cell.imageView!.image = developerImage
+        
+        return cell
+        
+        
+    }
 
 }
 
