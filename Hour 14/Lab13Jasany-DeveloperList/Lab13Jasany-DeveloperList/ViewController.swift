fffa7afb62cc834722cb1fc937cb04a57f132b48
Ryan <ryanjasany6@gmail.com>
2018-04-22 10:19:22 -0400

    load in developer data

--- Hour 14/Lab13Jasany-DeveloperList/Lab13Jasany-DeveloperList/ViewController.swift
+++ Hour 14/Lab13Jasany-DeveloperList/Lab13Jasany-DeveloperList/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     let webDevelopers: [String] = ["Al Dente", "Armand Hammer", "Barry Cade", "Cara Van"]
     let mobileDevelopers: [String] = ["Ginger Ayle", "June Bugg", "Iona Ford", "Sue Flay", "Tim Burr", "Will Wynn"]
     
+    let webDevelopersTitle: [String] = ["Full Stack", "ASP.NET", "PHP", "Node.js, Angular, and React"]
+    
+    let mobileDevelopersTitle: [String] = ["iOS with Swift", "iOS and Android", "iOS with Objective-C", "iOS with Swift and Objective-C", "Android", "Ionic"]
+    
+    let webDevelopersDetails: [String] = ["A full stack developer capable of front-end and back-end development in a Windows environment.", "Back-end developer focused on database applications using a variety of DBMSs.", "Several years experience writing PHP with database connectvity using Linux and Windows.", "Newby developer with great demonstration of skills using Node.js and Angular. Some recent work in React."]
+    
+    let mobileDevelopersDetails: [String] = ["12 apps developed using Swift and Xcode", "Senior app developer with experience in Android and iOS development", "Maintains large code base still in Objective-C. Working to learn Swift.", "Staff expert in Objective-C. Lead efforts to upgrade from Objective-C to Swift.", "Android expert with experience raning to days of Eclipse to current Android Studio versions.", "Prototyping go-to. Uses Ionic to spin up fast prototypes for all apps." ]
+    
     let kSectionCount:Int = 2
     let kWebSection:Int = 0
     let kMobileSection:Int = 1
