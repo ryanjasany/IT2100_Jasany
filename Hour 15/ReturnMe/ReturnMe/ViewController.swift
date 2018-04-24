abda1528386b2590d03abe76b6052908313375a7
Ryan <ryanjasany6@gmail.com>
2018-04-24 18:10:44 -0400

    implement settings bundle

--- Hour 15/ReturnMe/ReturnMe/ViewController.swift
+++ Hour 15/ReturnMe/ReturnMe/ViewController.swift
@@ -23,6 +23,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setValuesFromPreferences()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +32,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setValuesFromPreferences(){
+        
+        let userDefaults:UserDefaults=UserDefaults.standard
+        
+        let initialDefaults:[String:String]=[kPicture:"Dog",
+                                             kName:"Your Name",
+                                             kEmail:"you@you.com",
+                                             kPhone:"(555)555-1212"                                           ]
+        userDefaults.register(defaults: initialDefaults)
+        
+        let picturePreference:String = userDefaults.string(forKey: kPicture)!
+        
+        if picturePreference == "Dog"{
+            image.image = UIImage(named: "dog1.png")
+        }else if picturePreference == "Crazy Dog"
+        {
+                image.image = UIImage(named:"dog2.png")
+                
+        }else{
+                image.image = UIImage(named:"coral.png")
+        }
+        
+        name.text = userDefaults.string(forKey: kName)
+        email.text = userDefaults.string(forKey: kEmail)
+        phone.text = userDefaults.string(forKey: kPhone)
+            
+        
+        
+        
+        
+    }
 
 }
 
